3ad274b93b6471e0953ad4d8e456d607d5158551
Nathan <lenguyenthanhnhan590@gmail.com>
2025-10-20 17:20:14 +0700

    add inline diagnostic.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,55 +1,63 @@
 return {
-   {
-      "williamboman/mason.nvim",
-      config = function()
-         require("mason").setup()
-      end,
-   },
-   {
-      "williamboman/mason-lspconfig.nvim",
-      config = function()
-         require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls", "lemminx" },
-         })
-      end,
-   },
-   {
-      "neovim/nvim-lspconfig",
-      config = function()
-         local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "lemminx" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-         -- Lua LS
-         vim.lsp.config.lua_ls.setup({
-            cmd = { "lua-language-server" }, -- mason provides this
-            capabilities = capabilities,
-            settings = {
-               Lua = {
-                  diagnostics = {
-                     globals = { "vim" },
-                  },
-                  workspace = {
-                     library = vim.api.nvim_get_runtime_file("", true),
-                     checkThirdParty = false,
-                  },
-                  telemetry = {
-                     enable = false,
-                  },
-               },
-            },
-         })
+			-- Lua LS
+			lspconfig.lua_ls.setup({
+				cmd = { "lua-language-server" },
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+						telemetry = { enable = false },
+					},
+				},
+			})
 
-         -- XML LS (lemminx)
-         vim.lsp.config.lemminx.setup({
-            cmd = { "lemminx" },
-            capabilities = capabilities,
-            filetypes = { "xml", "xsd", "xsl", "xslt" },
-            settings = {
-               xml = {
-                  format = { enabled = true },
-                  validate = { enabled = true },
-               },
-            },
-         })
-      end,
-   },
+			-- XML LS (lemminx)
+			lspconfig.lemminx.setup({
+				cmd = { "lemminx" },
+				capabilities = capabilities,
+				filetypes = { "xml", "xsd", "xsl", "xslt" },
+				settings = {
+					xml = {
+						format = { enabled = true },
+						validate = { enabled = true },
+					},
+				},
+			})
+
+			-- Diagnostic settings (like Error Lens)
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
+		end,
+	},
 }
